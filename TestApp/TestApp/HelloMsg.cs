1a48454d2abf2ca9dfe1dde2a9b1f0d3c70e6e4d
malmsteen08 <neziryurekli@gmail.com>
2014-04-02 22:03:04 +0300

    Added TestApp

---
+++ TestApp/TestApp/HelloMsg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+
+    class TestApp
+    {
+
+        static void Main()
+        
+        {
+            HelloMsg h = new HelloMsg();
+            h.Speak();
+        }
+
+    }
+
+}
---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    class HelloMsg
+    {
+        public void Speak()
+        {
+            MessageBox.Show("Hello...");
+        }
+    }
+}
