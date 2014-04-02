697ab5b977d666af579bcbdd236b485b1e2a609d
malmsteen08 <neziryurekli@gmail.com>
2014-04-02 21:53:25 +0300

    Added ArrayList

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayList
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            SimpleArrays();
+            Console.ReadLine();
+        
+        }
+
+        private static void SimpleArrays()
+        {
+
+            Console.WriteLine("Simple Array Creation.");
+
+            var MyInts = new[3];
+            MyInts[0] = 100;
+            MyInts[1] = 200;
+            MyInts[2] = 300;
+
+            foreach (int i in MyInts)
+            Console.WriteLine(i);
+        
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
