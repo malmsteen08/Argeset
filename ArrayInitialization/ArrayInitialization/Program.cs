99255d676014a30b61769d7c7fd02dd19772c83e
malmsteen08 <neziryurekli@gmail.com>
2014-04-02 21:39:54 +0300

    added ArrayInitialization

---
+++ ArrayInitialization/ArrayInitialization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayInitialization
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            var stringArray = new[] {"one", "two", "three"};
+            Console.WriteLine("stringArray has {0} elements", stringArray.Length);
+
+            var boolArray = new[] 
+                { true, false, true };
+            Console.WriteLine("boolArray has {0} elements", boolArray.Length);
+
+            var integerArray = new[] 
+                { 1, 2, 3 };
+            Console.WriteLine("integerArray has {0} elements", integerArray.Length);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
