cb32e962c3eb8de869bb4257d608f15a34c41075
malmsteen08 <neziryurekli@gmail.com>
2014-04-02 21:55:34 +0300

    Added Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Calculator
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+
+            Calc c = new Calc();
+            int ans = c.Add(10, 84);
+            Console.WriteLine("10 + 84 = {0}", ans);
+            Console.ReadLine();
+
+        }
+    }
+
+    class Calc
+    {
+
+        public int Add(int x, int y)
+        {
+
+            return x + y;
+
+        }
+
+    }
+
+}
+
