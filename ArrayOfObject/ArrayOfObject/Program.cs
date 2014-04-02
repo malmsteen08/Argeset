2c7137ba090a26ddae9400221ad88943217db6f1
malmsteen08 <neziryurekli@gmail.com>
2014-04-02 21:51:37 +0300

    Added ArrayOfObject

---
+++ ArrayOfObject/ArrayOfObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayOfObject
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Array Of Object");
+
+            //var objectArray = new[]
+            object[] objectArray = new object[]
+                {10, false, new DateTime(01,02,1951), "Form & Void"};
+
+            foreach (object obj in objectArray)
+            {
+
+                Console.WriteLine("Type : {0}, Value : {1}", obj.GetType(), obj);
+            
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
